4b53a9e9135e5b39d97c0ceffe31ff8fa717e0e3
Mike Johnson <mike@failsafedesign.com>
2015-04-25 12:19:55 -0700

    add users

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all
+
+    render json: @products
+  end
+
+  def show
+    render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      head :no_content
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+
+    head :no_content
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :description)
+    end
+end
---
+++ db/migrate/20150425190338_add_accses_token_to_user.rb
@@ -0,0 +1,6 @@
+class AddAccsesTokenToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :access_token, :string
+    add_column :users, :username, :string
+  end
+end
